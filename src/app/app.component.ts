cba8be1ca95fe504be9b3dd36536a5f31fe2cf22
Adam Bethlehem <adam.bethlehem@safetyio.com>
2025-10-21 09:13:50 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+import {Header} from "./header/header";
+import { User } from './user/user';
+import { DUMMY_USERS } from './user/dummy-users';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    Header,
+    User
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+}
